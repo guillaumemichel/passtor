b452eab2e190c3d89b6ec5f3454de40823d65c00
Xavier Pantet <25949462+xavierpantet@users.noreply.github.com>
2020-01-23 15:12:24 +0100

    First steps of CLI

--- client/cli.go
+++ client/cli.go
@@ -4,4 +4,7 @@ import "../../passtor"
 
 func LaunchCrazyCLI(account *passtor.AccountClient) {
 
+	for {
+		action := PromptUser("What would you like to do?\n - Add a new login\n - Update an existing login\n - Delete an existing login")
+	}
 }
